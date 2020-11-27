e9376493d8eecff2fba45906e00594a09a0330f5
xuan567 <lixuan@xiyou3g.com>
2020-11-27 23:22:53 +0800

    leetcode旋转矩阵

---
+++ 数组和字符串/leetcode旋转矩阵.java
@@ -0,0 +1,38 @@
+package 数组和字符串;
+
+public class leetcode旋转矩阵 {
+    /*
+    * 给你一幅由 N × N 矩阵表示的图像，其中每个像素的大小为 4 字节。请你设计一种算法，将图像旋转 90 度。
+        不占用额外内存空间能否做到？
+
+        示例 1:
+        给定 matrix =
+        [
+          [1,2,3],
+          [4,5,6],
+          [7,8,9]
+        ],
+
+        原地旋转输入矩阵，使其变为:
+        [
+          [7,4,1],
+          [8,5,2],
+          [9,6,3]
+        ]*/
+
+    public void rotate(int[][] matrix) {
+        int n = matrix.length;
+        for (int i = 0; i < n; i++) {
+            for (int j = i; j < n; j++) {
+                int tmp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = tmp;
+            }
+            for (int j = 0; j < n / 2; j++) {
+                int tmp = matrix[i][j];
+                matrix[i][j] = matrix[i][n - j - 1];
+                matrix[i][n - j - 1] = tmp;
+            }
+        }
+    }
+}
