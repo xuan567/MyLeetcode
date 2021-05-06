4b0913e821c3a3d399b0d5b00599d5b1c4972f7d
xuan567 <572351128@qq.com>
2021-05-06 21:43:30 +0800

    leetcode64最小路径和

--- 动态规划/leetcode64最小路径和.java
+++ 动态规划/leetcode64最小路径和.java
@@ -16,7 +16,30 @@ public class leetcode64最小路径和 {
 
     * */
 
-    public int minPathSum(int[][] grid) {
+    //思路：
+    // 这道题不需要建立dp矩阵浪费空间，可以直接遍历grid
+    // 因为每次只能向右或者向下，所以走到该格子的最小路径=Math.min(grid[i - 1][j], grid[i][j - 1]) + grid[i][j]
+    // 每次判断，当i==0 && j==0 ,跳出
+    // 当i==0 && j!=0 , grid[i][j] = grid[i][j-1]+grid[i][j];
+    // 当j==0 && i!=0 , grid[i][j] = grid[i-1][j]+grid[i][j];
+    // 最后return grid[m-1][n-1]
 
+    public int minPathSum(int[][] grid) {
+        int m = grid.length;
+        int n = grid[0].length;
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(i==0 && j==0){
+                    continue;
+                }else if(i==0){
+                    grid[i][j] = grid[i][j-1]+grid[i][j];
+                }else if(j==0){
+                    grid[i][j] = grid[i-1][j]+grid[i][j];
+                }else {
+                    grid[i][j] = Math.min(grid[i - 1][j], grid[i][j - 1]) + grid[i][j];
+                }
+            }
+        }
+        return grid[m-1][n-1];
     }
 }
